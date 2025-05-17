df5beef0e5075abab0b0ecdd7cb73ccc60b07655
Michael Giuffrida <1449368+mgiuffrida@users.noreply.github.com>
2025-05-16 22:35:59 -0500

    Update workspaces and config

---
+++ workspaces/util/cli.js
@@ -0,0 +1,44 @@
+import readline from "node:readline";
+import process from "node:process";
+
+export class Cli {
+  constructor(usageMessage) {
+    this._usageMessage = usageMessage;
+  }
+
+  usage(message = "") {
+    if (message) {
+      console.error(message + "\n");
+    }
+    console.log(this._usageMessage);
+    process.exit(message ? 1 : 0);
+  }
+}
+
+/**
+ * @param {string} question
+ * @return {Promise<string>}
+ */
+export function prompt(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) =>
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    }),
+  );
+}
+
+/**
+ * @param question
+ * @returns {Promise<boolean>}
+ */
+export function confirm(question) {
+  return prompt(question + " (y/N) ").then((answer) => {
+    return ["y", "yes"].includes(answer.trim().toLowerCase());
+  });
+}
