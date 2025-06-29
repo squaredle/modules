0b27163633102164aa6d7d0c567449ac64ff75a1
Michael Giuffrida <1449368+mgiuffrida@users.noreply.github.com>
2025-06-29 14:53:02 -0500

    Update cli to optionally take a function to orchestrate confirmation promises

--- workspaces/util/cli.js
+++ workspaces/util/cli.js
@@ -1,5 +1,6 @@
 import readline from "node:readline";
 import process from "node:process";
+import { program } from "commander";
 
 export class Cli {
   constructor(usageMessage) {
@@ -15,22 +16,32 @@ export class Cli {
   }
 }
 
+let promptManager = {
+  runSync: (fn) => fn(),
+};
+
+export function setPromptManager(manager) {
+  promptManager = manager;
+}
+
 /**
  * @param {string} question
  * @return {Promise<string>}
  */
-export function prompt(question) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+export async function prompt(question) {
+  return promptManager.runSync(() => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  return new Promise((resolve) =>
-    rl.question(question, (answer) => {
-      rl.close();
-      resolve(answer);
-    }),
-  );
+    return new Promise((resolve) =>
+      rl.question(question, (answer) => {
+        rl.close();
+        resolve(answer);
+      }),
+    );
+  });
 }
 
 /**
@@ -38,12 +49,46 @@ export function prompt(question) {
  * @param {boolean=} assumeTrue Make true the default if no answer is given
  * @returns {Promise<boolean>}
  */
-export function confirm(question, assumeTrue) {
-  const msg = `${question} (${assumeTrue ? "Y/n" : "y/N"}) `;
-  return prompt(msg).then((answer) => {
-    if (answer === "") {
-      return assumeTrue === true;
-    }
-    return ["y", "yes"].includes(answer.trim().toLowerCase());
+export async function confirm(question, assumeTrue) {
+  return promptManager.runSync(() => {
+    const msg = `${question} (${assumeTrue ? "Y/n" : "y/N"}) `;
+    return prompt(msg).then((answer) => {
+      if (answer === "") {
+        return assumeTrue === true;
+      }
+      return ["y", "yes"].includes(answer.trim().toLowerCase());
+    });
   });
+}
+
+/**
+ * Converts an array of key=value pairs into an object.
+ * @param {Array<string>=} args
+ * @returns {Record<string, string>}
+ */
+export function splitKeyValueArgs(args = []) {
+  const obj = {};
+  for (const pair of args) {
+    // Split on the first `=`, allowing for `=` in values.
+    const [key, ...valueParts] = pair.split("=");
+    const value = valueParts.join("=");
+    if (!key || !value) {
+      program.error(`Invalid key-value pair: ${pair}`);
+    }
+    if (key in obj) {
+      program.error(`Duplicate key: ${key}`);
+    }
+    obj[key] = value;
+  }
+  return obj;
+}
+
+export const ScreenCodes = {
+  reset: "\x1b[0m",
+  underline: "\x1b[4m",
+};
+if (!process.stdout.isTTY) {
+  for (const code in ScreenCodes) {
+    ScreenCodes[code] = "";
+  }
 }
